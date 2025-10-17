fb0a696e773e35034c6f696e0981814f9e005694
JesusLugo2002 <jesusdlugo02@gmail.com>
2025-10-17 19:36:56 +0100

    Add list-group

--- docs/src/App.vue
+++ docs/src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import Hero from './Components/Hero.vue';
+import Hero from './Components/Header/Hero.vue';
 </script>
 
 <template>
---
+++ docs/src/Components/Header/ContentsList.vue
@@ -0,0 +1,15 @@
+<template>
+  <ul class="list-group list-group-flush text-white fw-bold">
+    <a href="" class="list-group-item list-group-item-action">La tienda de Pierre</a>
+    <a href="" class="list-group-item list-group-item-action">Odoo como ERP elegido</a>
+    <a href="" class="list-group-item list-group-item-action">Módulos utilizados</a>
+    <a href="" class="list-group-item list-group-item-action">Problemas resueltos</a>
+    <a href="" class="list-group-item list-group-item-action">Autores</a>
+  </ul>
+</template>
+
+<style scoped>
+.list-group {
+  background-color: transparent;
+}
+</style>
