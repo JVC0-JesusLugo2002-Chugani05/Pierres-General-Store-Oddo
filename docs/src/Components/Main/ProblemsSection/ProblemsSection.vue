e1ed07862bc33c7806df8744bc98601fdc6c9c5f
PRORIX <romengilberto08@gmail.com>
2025-10-22 20:28:42 +0100

    Progress with problem section

--- docs/src/Components/Main/ProblemsSection/ProblemsSection.vue
+++ docs/src/Components/Main/ProblemsSection/ProblemsSection.vue
@@ -7,13 +7,13 @@ const problems = [
     title: 'Administración de los clientes',
     imgUrl: 'https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut voluptas perspiciatis nisi quia non quos, dicta, neque dolores, rem libero vero consequuntur incidunt. Incidunt odit qui, explicabo eius atque illo.',
+      'En la tienda de Pierre, uno de los pilares más importantes de su negocio es la cercanía que tiene con sus clientes, para mantener ello, se asegura de registrar sus cumpleaños en el calendario y de ofrecer descuentos en ellos para afianzar sus relaciones.',
   },
   {
     title: 'Control del flujo de compras y ventas',
     imgUrl: 'https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut voluptas perspiciatis nisi quia non quos, dicta, neque dolores, rem libero vero consequuntur incidunt. Incidunt odit qui, explicabo eius atque illo.',
+      'La tienda trabaja con productos que entran y salen del inventario al día',
   },
   {
     title: 'Gestión del inventario',
