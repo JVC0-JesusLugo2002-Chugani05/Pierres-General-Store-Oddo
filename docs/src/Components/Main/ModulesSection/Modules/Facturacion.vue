f3e3fb5dd3d8a0d790efffdb80dbf55993c2571a
JesusLugo2002 <jesusdlugo02@gmail.com>
2025-10-24 11:57:19 +0100

    Add Figure component to refactor images + text combination of modules card

--- docs/src/Components/Main/ModulesSection/Modules/Facturacion.vue
+++ docs/src/Components/Main/ModulesSection/Modules/Facturacion.vue
@@ -1,35 +1,17 @@
-<template>
-  <div class="card-body fs-5 text-dark p-3">
-    <img
-      src="/img/modules-modals/facturacion/vista-general.png"
-      alt="Vista general de facturacion"
-      class="img-fluid figure-img"
-    />
-    <p class="figure-caption text-end">Vista general de facturas de clientes</p>
+<script setup>
+import Figure from './Figure.vue';
+</script>
 
+<template>
+  <div class="card-body fs-5 text-dark p-5">
+    <Figure imgUrl="/img/modules-modals/facturacion/vista-general.png" description="Vista general de facturas de clientes"/>
     <p>
       Este módulo facilita la creación y personalización de facturas gracias a la disposición de
       elementos esenciales como la información del cliente, productos, precios y demás. Con este
       módulo, Pierre puede emitir facturas, actualizar estados de pagos o incluso crear pagos en
       lote para las facturas de sus proveedores, los granjeros de Pelican Town.
     </p>
-
-    <div class="row my-5 d-flex flex-column align-items-center">
-      <img
-        src="/img/modules-modals/facturacion/control-factura-proveedor.png"
-        alt="Control de una factura de proveedor"
-        class="img-fluid figure-img col-9"
-      />
-      <p class="figure-caption text-center">Detalles de una factura de proveedor</p>
-    </div>
-
-    <div class="row d-flex flex-column align-items-center">
-      <img
-        src="/img/modules-modals/facturacion/analisis-facturas.png"
-        alt="Análisis de factura"
-        class="img-fluid figure-img col-9"
-      />
-      <p class="figure-caption text-center">Informe de análisis de facturas</p>
-    </div>
+    <Figure imgUrl="/img/modules-modals/facturacion/control-factura-proveedor.png" description="Detalles de una factura de proveedor"/>
+    <Figure imgUrl="/img/modules-modals/facturacion/analisis-facturas.png" description="Informe de análisis de facturas"/>
   </div>
 </template>
