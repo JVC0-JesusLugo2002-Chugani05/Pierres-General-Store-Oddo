8a093e9bb743049555b6220a8fce699102b3e716
JesusLugo2002 <jesusdlugo02@gmail.com>
2025-10-24 11:59:54 +0100

    Add Empleados module card

--- docs/src/Components/Main/ModulesSection/Modules/Empleados.vue
+++ docs/src/Components/Main/ModulesSection/Modules/Empleados.vue
@@ -1,5 +1,10 @@
+<script setup>
+import Figure from './Figure.vue';
+</script>
+
 <template>
-  <div class="card-body fs-5 text-dark p-3">
+  <div class="card-body fs-5 text-dark p-5">
+    <Figure imgUrl="/img/modules-modals/empleados/vista-general.png" description="Vista general de empleados" />
     <p>
       La valiosa centralización de los procesos de recursos humanos es lo que ofrece este módulo.
       Pierre tendrá gracias a esto una vista general de los departamentos de su negocio, una clara
@@ -7,5 +12,7 @@
       los ajustes de acceso restringido y un control de las solicitudes de permisos y los plazos de
       ausencia.
     </p>
+    <Figure imgUrl="/img/modules-modals/empleados/departamentos.png" description="Organigrama de departamentos" />
+    <Figure imgUrl="/img/modules-modals/empleados/detalles-empleado.png" description="Detalles de un empleado" />
   </div>
 </template>
