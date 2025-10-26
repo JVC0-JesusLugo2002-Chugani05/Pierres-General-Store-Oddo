908a278edb39cbac693c2a11ebad890df844b131
JesusLugo2002 <jesusdlugo02@gmail.com>
2025-10-26 01:06:05 +0100

    Add content to CRM module

--- docs/src/Components/Main/ModulesSection/Modules/CRM.vue
+++ docs/src/Components/Main/ModulesSection/Modules/CRM.vue
@@ -1,5 +1,10 @@
+<script setup>
+import Figure from './Figure.vue';
+</script>
+
 <template>
-  <div class="card-body fs-5 text-dark p-3">
+  <div class="card-body fs-5 text-dark p-5">
+    <Figure imgUrl="crm/vista-general.png" description="Vista general del módulo"/>
     <p>
       La aplicación de CRM es de las herramientas más útiles para el negocio de Pierre, pues es la
       que le permite enfocarse en sus clientes y necesidades de ventas, llevando el seguimiento de
@@ -7,5 +12,7 @@
       plataforma, puede programar llamadas, envios de presupuestos y reuniones con facilidad
       manteniendo una comunicación fluida y eficaz.
     </p>
+    <Figure imgUrl="crm/pronostico.png" description="Pronóstico y resumen de las oportunidades por fechas "/>
+    <Figure imgUrl="crm/recurrentes.png" description="Capacidad de crear oportunidades recurrentes"/>
   </div>
 </template>
